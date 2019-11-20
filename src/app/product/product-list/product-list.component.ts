35c4707c0b9978376516e554789c9a35e02b17bf
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2019-11-20 17:17:29 +0900

    update product-list Implement
    商品リストの表示を実装

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/shared/models/product';
 
 @Component({
     selector: 'app-product-list',
@@ -8,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
     public id: number;
     id2: number;
+
+    products: Product[] = null;
     constructor() {}
 
     ngOnInit() {
         this.id = 15;
         this.id2 = 10;
+
+        setTimeout(() => {
+            this.products = [
+                new Product(
+                    1,
+                    'テスト商品',
+                    3800,
+                    'テストで追加した商品です。'
+                ),
+                new Product(
+                    2,
+                    'AngularDesignPattern',
+                    4000,
+                    'Angularでデザインパターンを極めるための指南書'
+                ),
+                new Product(
+                    3,
+                    'angularQuest',
+                    640,
+                    'Angularを極めるため旅にでる少年、その先に待つものとは…。'
+                ),
+            ];
+        }, 1000);
     }
 }
